6e9bdc8815aa7d20cb1345bba4208c3159a19bd5
ChuckLeon <charles_heon@hotmail.com>
2023-03-10 05:07:25 -0500

    fix: can now delete correctly dice

--- components/Dice.vue
+++ components/Dice.vue
@@ -1,83 +1,86 @@
 <template>
-    <div class="dice-container" :class="diceClass">
-        <div class="dice" @click="roll">
-            <div class="content">
-                <h3 class="text">D{{ max }}</h3>
-                <div :class="{numberContainer: true, rolling: isRolling}">
-                    <p :class="{number: true, active: item === number}" v-for="item in shuffledArray" :key="item">{{ item }}</p>                    
-                </div>
-            </div>            
+  <div class="dice-container" :class="{ active: isActive }">
+    <div class="dice" @click="roll">
+      <div class="content">
+        <h3 class="text">D{{ max }}</h3>
+        <div class="numberContainer" :class="{ rolling: isRolling }">
+          <p
+            :class="{ number: true, active: item === number }"
+            v-for="item in shuffledArray"
+            :key="item"
+          >
+            {{ item }}
+          </p>
         </div>
-        <button class="clear btn" @click="remove">x</button>
+      </div>
     </div>
+    <button class="clear btn" @click="remove">x</button>
+  </div>
 </template>
 
 <script>
 export default {
-    props:{
-        id: {
-            type: [String, Number],
-            required: true,
-        },
-        max: {
-            type: [String, Number],
-            required: true,
-        },
+  props: {
+    id: {
+      type: [String],
+      required: true,
+    },
+    max: {
+      type: [String, Number],
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      number: 0,
+      isRolling: false,
+      array: Array.from({ length: this.max }, (_, i) => i + 1),
+      isActive: false,
+    };
+  },
+
+  computed: {
+    shuffledArray() {
+      return this.array.sort((a, b) => 0.5 - Math.random());
+    },
+    rollTime() {
+      return Math.min(this.max * 300 + 300, 2000);
+    },
+  },
+
+  methods: {
+    roll() {
+      //reset dice
+      this.number = 0;
+      this.isRolling = true;
+
+      var byteArray = new Uint8Array(1);
+      window.crypto.getRandomValues(byteArray);
+
+      var range = this.max;
+      var max_range = 256;
+      if (byteArray[0] >= Math.floor(max_range / range) * range) this.roll();
+
+      setTimeout(() => {
+        this.isRolling = false;
+        this.number = parseInt(1 + (byteArray[0] % range));
+      }, this.rollTime);
     },
 
-    data(){
-        return{
-            number: 0,
-            isRolling: false,
-            array: Array.from({length: this.max}, (_, i) => i + 1),     
-            diceClass: '',       
-        }
+    remove() {
+      this.isActive = false;
+
+      setTimeout(() => {
+        this.$emit("removeDice", this.id);
+      }, 300);
     },
+  },
 
-    computed:{
-        shuffledArray(){
-            return this.array.sort((a, b) => 0.5 - Math.random());
-        },
-        rollTime(){
-            return Math.min(this.max * 300 + 300, 2000);
-        }
-    },
-
-    methods:{
-        roll(){       
-            //reset dice
-            this.number = 0;
-            this.isRolling = true;     
-
-            var byteArray = new Uint8Array(1);
-            window.crypto.getRandomValues(byteArray);
-
-            var range = this.max;
-            var max_range = 256;
-            if(byteArray[0] >= Math.floor(max_range/range) * range)
-                this.roll();
-            
-            setTimeout(() => {
-                this.isRolling = false;
-                this.number = parseInt(1 + (byteArray[0] % range));                
-            }, this.rollTime);                                         
-        },
-
-        remove(){            
-            this.diceClass = '';
-
-            setTimeout(() => {
-                this.$emit('removeDice', this.id);
-            }, 300);
-        }
-    },
-
-    mounted() {
-        this.$on('submit', this.submit)
-
-        setTimeout(() => {
-            this.diceClass = 'active';
-        }, 100);
-    },
-}
+  mounted() {
+    setTimeout(() => {
+      this.isActive = !this.isActive;
+    }, 100);
+  },
+};
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="home">
     <Tray @addToGame="addDiceToGame" />
-    
+
     <div class="home__content">
       <h1 class="title">Gelatinous Dice</h1>
-      
-      <div :class="'grid grid--' + gridLength ">
-        <Dice v-for="(dice, index) in diceToRoll" :key="index" 
-          :id="index" 
-          :max="dice" 
-          :ref="index"
-          @removeDice="removeDiceFromGame" />           
+
+      <div :class="'grid grid--' + gridLength">
+        <Dice
+          v-for="dice in diceToRoll"
+          :key="dice.id"
+          :id="dice.id"
+          :max="dice.max"
+          @removeDice="removeDiceFromGame"
+        />
       </div>
 
-      <div class="btn game-roller" @click="rollAllDice" v-if="diceToRoll.length > 0">
+      <div
+        class="btn game-roller"
+        @click="rollAllDice"
+        v-if="diceToRoll.length > 0"
+      >
         Roll all!
       </div>
     </div>
@@ -21,42 +27,70 @@
 </template>
 
 <script>
-import Dice from '../components/Dice.vue';
-import Tray from '../components/tray/Tray.vue';
+import Dice from "../components/Dice.vue";
+import Tray from "../components/tray/Tray.vue";
 export default {
-  head:{
-    title: 'Gelatinous Dice',
+  head: {
+    title: "Gelatinous Dice",
   },
 
   components: { Dice, Tray },
 
-  data(){
-    return{
-      diceToRoll:[], //will be managed by the Tray
+  data() {
+    return {
+      diceToRoll: [], //will be managed by the Tray
       gridMaxLength: 4,
-    }
+    };
   },
 
-  computed:{
-    gridLength(){
-      return this.diceToRoll.length < this.gridMaxLength ? this.diceToRoll.length : this.gridMaxLength;
-    }
+  computed: {
+    gridLength() {
+      return this.diceToRoll.length < this.gridMaxLength
+        ? this.diceToRoll.length
+        : this.gridMaxLength;
+    },
   },
 
-  methods:{
-    addDiceToGame(number){          
-      this.diceToRoll.push(number);      
+  methods: {
+    addDiceToGame(number) {
+      this.diceToRoll.push({ id: this.generateGuid(), max: number });
     },
 
-    removeDiceFromGame(id){                
-      this.diceToRoll.splice(id, 1);
+    removeDiceFromGame(id) {
+      const newDiceToRoll = this.diceToRoll.filter((dice) => dice.id !== id);
+      this.diceToRoll = newDiceToRoll;
     },
 
-    rollAllDice(){
+    rollAllDice() {
       this.diceToRoll.forEach((dice, index) => {
         this.$refs[index][0].roll();
       });
-    }
-  }
-}
+    },
+
+    generateGuid() {
+      var d = new Date().getTime(); //Timestamp
+      var d2 =
+        (typeof performance !== "undefined" &&
+          performance.now &&
+          performance.now() * 1000) ||
+        0; //Time in microseconds since page-load or 0 if unsupported
+      return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+        /[xy]/g,
+        function (c) {
+          var r = Math.random() * 16; //random number between 0 and 16
+          if (d > 0) {
+            //Use timestamp until depleted
+            r = (d + r) % 16 | 0;
+            d = Math.floor(d / 16);
+          } else {
+            //Use microseconds since page-load if supported
+            r = (d2 + r) % 16 | 0;
+            d2 = Math.floor(d2 / 16);
+          }
+          return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+        }
+      );
+    },
+  },
+};
 </script>
